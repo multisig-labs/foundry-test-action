5d5b33553b7896783242a1b0b3f441c833fdaf45
chand1012 <chandler@chand1012.dev>
2022-04-27 14:30:02 -0400

    Update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# if watch or -w is passed at all
-# exit with an error code
-if [[ "$@" == *"-w"* ]] || [[ "$@" == *"--watch"* ]]; then
-  echo "Cannot use watch mode with an action."
-  exit 1
-fi
-
-forge test "$@"
+forge test 
